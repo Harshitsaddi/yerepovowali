a09f33fe88c4e19b51a8199bad310b79deb5a855
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2024-10-22 14:37:50 +0530

    fourth-22Feb

--- FEE-2(Project-E-1)/js/index.js
+++ FEE-2(Project-E-1)/js/index.js
@@ -5,10 +5,20 @@ const cityAbout =  document.querySelectorAll(".cityAbout");
 const citySearchBox = document.getElementById('citySearch');
 const nextBtn = document.getElementById('nextBtn');
 const video = document.getElementById('video');
+const movieName = document.getElementById('movie-name');
+const movieDesc = document.getElementById('movie-desc');
+const container = document.getElementById('container');
 
-const movies = [2, 3, 4];// movie-Trailer names
 
-let index = 2;
+const movies = [
+    { index: 1, summary: `Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.`, title:"Deadpool And Wolverine" },
+    { index: 2, summary: "A soldier returns home from war to find his girlfriend missing, family broken, and neighborhood overrun by crime. Though reluctant, he becomes a hitman to protect loved ones.", title:"Gadar2" },
+    { index: 3,  summary:"A prison warden recruits inmates to commit outrageous crimes that shed light on corruption and injustice - and that lead him to an unexpected reunion.", title:"Jawan"},
+    { index: 4, title: "Avatar The way of water", summary: "Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home."},
+    { index: 5, title:"Stree2", summary: "After the events of Stree, the town of Chanderi is being haunted again. This time, women are mysteriously abducted by a terrifying headless entity. Once again, it's up to Vicky and his friends to save their town and loved ones."}
+];
+
+let index = 1;
 
 crossBtn.addEventListener("click", () => {
     city.style.display = "none";
@@ -43,11 +53,33 @@ citySearchBox.addEventListener("input", () => {
 
 nextBtn.addEventListener("click", () => {
     if (index == 5) {
-        index = 1;
-        video.src = `../Files/vedio/${index}.mp4`;
+        index = 0;
+        video.src = `../Files/vedio/${movies[index].index}.mp4`;
+        movieName.innerHTML = movies[index].title;
+        movieDesc.innerHTML = movies[index].summary;
     }
     else {
-        video.src = `../Files/vedio/${index}.mp4`;
+        video.src = `../Files/vedio/${movies[index].index}.mp4`;
+        movieName.innerHTML = movies[index].title;
+        movieDesc.innerHTML = movies[index].summary;
+
     }
+    console.log(`../Files/vedio/${movies[index].index}.mp4`);
     index++;
+});
+
+container.addEventListener("mouseover", () => {
+    nextBtn.style.display = "block";
+});
+
+container.addEventListener("mouseout", () => {
+    nextBtn.style.display = "none";
+    console.log("hello");
+
+});
+
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime == video.duration) {
+        video.play();
+    }
 });
